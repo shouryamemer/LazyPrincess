a825321c882f5cad1d138952e0acde71c4365cc1
shouryamemer <132538868+shouryamemer@users.noreply.github.com>
2023-08-30 11:45:20 -0700

    Update exceptions.py

--- server/exceptions.py
+++ server/exceptions.py
@@ -1,11 +1,11 @@
     # Credit @LazyDeveloper.
     # Please Don't remove credit.
-    # Born to make history @LazyDeveloper !
+    # Born to make history @SHOURYA !
 
     # Thank you LazyDeveloper for helping us in this Journey
-    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+    # 🥰  Thank you for giving me credit @wtfshourya  🥰
     # thanks Adarsh Goel for Server Routing
-    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+    # for any error please contact me -> telegram@wtfshourya or insta @shouryamemer 
 
 class InvalidHash(Exception):
     message = "Invalid hash"
